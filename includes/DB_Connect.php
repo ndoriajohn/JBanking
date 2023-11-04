9420fd27189a47c7110edb095afa23a135768a72
Benjamin Mundama <BMundama@strathmore.local>
2023-11-04 11:49:58 +0300

    DB connection made

---
+++ includes/DB_Connect.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "constants.php";
+    try{
+        $pdo = new PDO("mysql:host=$hostname; dbname=$dbname", $username, $userpass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected Successfully!!";
+    } catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
